bee1138c1d9ad6e9ea91c3d9d3034ec159b7283e
nagaohiroki <testplayers0226@gmail.com>
2019-10-01 16:58:13 +0900

    add python file

---
+++ tax.py
@@ -0,0 +1,6 @@
+price = 241
+food_tax = 1.0 + 0.08
+normal_tax = 1.0 + 0.1
+reduction_normal = 1.0 - 0.02
+reduction_sp = 1.0 - 0.05
+print("food_tax(8%) red_sp(2%):", price * food_tax * reduction_normal)
